2b68b4d3897908a8fc04f4a7f398314ba362ec02
Eduardo Leszczij <eduardocfc15@hotmail.com>
2022-07-19 11:43:55 -0300

    Alterações nos arquivos Payments

--- cypress/Pages/Payments.js
+++ cypress/Pages/Payments.js
@@ -17,11 +17,10 @@ class Payments{
         cy.get('#btn-login')
             .shadow()
             .find('[id="custom-button"]')
-            .click({force: true});
+            .click({ force: true });
         cy.wait(3000);
         cy.get('span[id="navbar-title"]')
             .should('have.text', 'Transações');
-
     };
 
     searchTransactionBypedidoId(transaction) {
@@ -32,11 +31,10 @@ class Payments{
         cy.get('#btn-search')
             .shadow()
             .find('[data-testid="btn-search"]')
-            .click({force: true})
+            .click({ force: true })
             .wait(3000);
         cy.contains('tbody tr td:nth-child(2)', transaction.pedidoId)
             .should('have.text', transaction.pedidoId);
-            
     };
 
     searchTransactionBycapturaId(transaction) {
@@ -47,61 +45,56 @@ class Payments{
         cy.get('#btn-search')
             .shadow()
             .find('[data-testid="btn-search"]')
-            .click({force: true})
+            .click({ force: true })
             .wait(3000);
         cy.contains('tbody tr td:nth-child(3)', transaction.capturaId)
             .should('have.text', transaction.capturaId);
-
     };
-    
+
     searchByPeriod(period, statusCode) {
         cy.get('div[class="calendar__wrapper"]').click();
         cy.wait(3000);
         cy.get('[idbutton="btn-previous-month"]')
             .shadow()
             .find('[data-testid="btn-previous-month"]')
-            .click({force: true})
+            .click({ force: true });
         cy.contains('span[class="calendar__number"]', period.days)
-            .click({multiple: true});
+            .click({ multiple: true });
         cy.wait(3000);
         cy.get('[idbutton="btn-apply-calendar"]')
             .shadow()
             .find('[data-testid="btn-apply-calendar"]')
-            .click({force: true});
+            .click({ force: true });
         cy.get('tbody td:nth-child(8)')
-            .should('have.text', statusCode.status)            
-
+            .should('have.text', statusCode.status);
     };
 
     searchSevenDaysAgo() {
 
         var days = '30 dias'
-        
+
         if (days === "7 dias") {
-            cy.get('[data-testid="period-7"]').click();                
+            cy.get('[data-testid="period-7"]').click();
         } else if (days === "15 dias") {
-            cy.get('[data-testid="period-15"]').click();  
+            cy.get('[data-testid="period-15"]').click();
         } else if (days === "30 dias") {
-            cy.get('[data-testid="period-30"]').click();  
+            cy.get('[data-testid="period-30"]').click();
         }
-           
-    }
+    };
 
     exportFile() {
-       
         cy.get('[idbutton="btn-export"]')
             .shadow()
             .find('[data-testid="btn-export"]')
-            .click({force: true});
+            .click({ force: true });
         cy.get('div[class="download-modal__wrapper"]')
             .should('have.text', 'Downlolad realizado');
-    }
+    };
 
     logout() {
-
         cy.get('div[id="div-sidebar-logout"]')
             .click()
-    }
+    };
 
 };
 
--- cypress/integration/Payments.spec.js
+++ cypress/integration/Payments.spec.js
@@ -3,23 +3,23 @@
 import pay from '../Pages/Payments'
 
 function clean() {
-    afterEach(function(){
+    afterEach(function () {
         cy.get('div[class="calendar__wrapper"]').click();
         cy.get('[data-testid="btn-clear-calendar"]').click();
         cy.get('[idbutton="btn-apply-calendar"]')
-        .shadow()
-        .find('[data-testid="btn-apply-calendar"]')
-        .click({force: true});        
-    })   
-}
+            .shadow()
+            .find('[data-testid="btn-apply-calendar"]')
+            .click({ force: true });
+    })
+};
 
 const period = {
     day: '27'
-}
+};
 
 const statusCode = {
     status: ' Paga '
-}
+};
 
 describe("Buscar Transação por ID Pedido/Captura", () => {
 
@@ -31,29 +31,26 @@ describe("Buscar Transação por ID Pedido/Captura", () => {
     const transaction = {
         pedidoId: '21347155',
         capturaId: 'af3sa1e2'
-    }
+    };
 
     it('search by pedidoId', () => {
-        
+
         pay.acess(user);
         pay.searchTransactionBypedidoId(transaction);
-
     });
 
     it('serach by capturaId', () => {
-        
+
         pay.acess(user);
         pay.searchTransactionBycapturaId(transaction);
-
     });
 
     it('Download File Export By Period', () => {
-        
+
         pay.acess(user);
         pay.searchByPeriod(period, statusCode);
         pay.exportFile();
         pay.logout();
-
     });
 
     it('search by period', () => {
@@ -61,12 +58,11 @@ describe("Buscar Transação por ID Pedido/Captura", () => {
         pay.acess(user);
         pay.searchByPeriod(period, statusCode);
         pay.clean();
-        
     });
 
     it.only('search by last 7 days', () => {
-       
+
         pay.acess(user);
         pay.searchSevenDaysAgo();
     });
-})
+});
